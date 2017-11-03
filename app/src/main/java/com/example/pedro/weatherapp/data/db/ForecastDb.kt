b4e0606228c7b6dafcf0947e9bb5da2c6534ba27
Pedro Roig <pedro.roig@tecnocom.es>
2017-11-03 15:15:50 +0100

    getting  data from DB [INCOMPLETE] [NO WORKING]

---
+++ app/src/main/java/com/example/pedro/weatherapp/data/db/ForecastDb.kt
@@ -0,0 +1,37 @@
+package com.example.pedro.weatherapp.data.db
+
+import org.jetbrains.anko.db.MapRowParser
+import org.jetbrains.anko.db.SelectQueryBuilder
+import org.jetbrains.anko.db.select
+
+class ForecastDb(
+    private var forecastDbHelper: ForecastDbHelper = ForecastDbHelper.instance,
+    private val dataMapper : DbDataMaper = DbDataMapper()) {
+
+    //function that requests a forecast based on a zip code and a date
+    fun requestForecastByZipCode(zipCode: Long, date: Long) = forecastDbHelper.use {
+        val dailyRequest = "${DayForecastTable.CITY_ID} = ? " +
+                "AND ${DayForecastTable.DATE} >= ?"
+
+        val dailyForecast = select(DayForecastTable.NAME)
+                .whereSimple(dailyRequest, zipCode.toString(), date.toString())
+                .parseList { DayForecast(HashMap(it)) }
+
+        val city = select(CityForecastTable.NAME)
+                .whereSimple("${CityForecastTable.ID} = ?", zipCode.toString())
+                .parseOpt{ CityForecast(HashMap(it), dailyForecast) }
+    }
+
+    fun <T : Any> SelectQueryBuilder.parseList(
+            parser: (Map<String, Any?>) -> T): List<T> =
+            parseList(object : MapRowParser<T> {
+                override fun parseRow(columns: Map<String, Any?>): T = parser(columns)
+            })
+
+    fun <T : Any> SelectQueryBuilder.parseOpt(
+            parser: (Map<String, Any?>) -> T): T? =
+            parseOpt(object : MapRowParser<T> {
+                override fun parseRow(columns: Map<String, Any?>): T = parser(columns)
+            })
+
+}
