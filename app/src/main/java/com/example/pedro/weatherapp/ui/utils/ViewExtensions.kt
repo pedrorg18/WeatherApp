e07387149d2d4d308dbc4ee44afcc62e2330d386
Pedro Roig <pedro.roig@tecnocom.es>
2017-11-24 16:49:09 +0100

    TextView extension property textColor

--- app/src/main/java/com/example/pedro/weatherapp/ui/utils/ViewExtensions.kt
+++ app/src/main/java/com/example/pedro/weatherapp/ui/utils/ViewExtensions.kt
@@ -2,6 +2,11 @@ package com.example.pedro.weatherapp.ui.utils
 
 import android.content.Context
 import android.view.View
+import android.widget.TextView
 
 val View.ctx: Context
     get() = context
+
+var TextView.textColor: Int
+    get() = currentTextColor
+    set(v) = setTextColor(v)
