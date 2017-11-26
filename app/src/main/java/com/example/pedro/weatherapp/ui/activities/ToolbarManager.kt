b1b33d59158eeacf0375a6a4ad784040bbe1351b
Pedro Roig <pedrorg18@hotmail.com>
2017-11-26 21:59:15 +0100

    interface with common behavior for the activities to manage the toolbar (lesson 24)

---
+++ app/src/main/java/com/example/pedro/weatherapp/ui/activities/ToolbarManager.kt
@@ -0,0 +1,52 @@
+package com.example.pedro.weatherapp.ui.activities
+
+import android.graphics.drawable.Drawable
+import android.support.v7.graphics.drawable.DrawerArrowDrawable
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import com.example.pedro.weatherapp.R
+import com.example.pedro.weatherapp.ui.App
+import com.example.pedro.weatherapp.ui.utils.ctx
+import com.example.pedro.weatherapp.ui.utils.slideEnter
+import com.example.pedro.weatherapp.ui.utils.slideExit
+import org.jetbrains.anko.toast
+
+interface ToolbarManager {
+
+    val toolbar: Toolbar
+
+    var toolbarTitle: String
+    get() = toolbar.title.toString()
+    set(v) { toolbar.title = v }
+
+    fun initToolbar() {
+        toolbar.inflateMenu(R.menu.menu_main)
+        toolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.action_settings -> App.instance.toast("Setings")
+                else -> App.instance.toast("Unknown Option")
+            }
+            true
+        }
+    }
+
+    fun enableHomeAsUp(up: () -> Unit) {
+        toolbar.navigationIcon = createUpDrawable()
+        toolbar.setNavigationOnClickListener{up()}
+    }
+
+    private fun createUpDrawable() =
+            with(DrawerArrowDrawable(toolbar.ctx)) {
+                progress = 1f
+                this
+            }
+
+    fun attachToScroll(recyclerView: RecyclerView) {
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                if (dy > 0) toolbar.slideExit() else toolbar.slideEnter()
+            }
+        })
+    }
+
+}
