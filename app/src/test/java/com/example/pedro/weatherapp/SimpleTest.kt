39bee3ed54a9f470be1ded7d33843f49c65e9785
Pedro Roig <pedro.roig@tecnocom.es>
2017-12-01 15:35:49 +0100

    dummy unit test (lesson 28)

---
+++ app/src/test/java/com/example/pedro/weatherapp/SimpleTest.kt
@@ -0,0 +1,11 @@
+package com.example.pedro.weatherapp
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SimpleTest {
+
+    @Test fun unitTestingWorks() {
+        assertTrue(true)
+    }
+}
