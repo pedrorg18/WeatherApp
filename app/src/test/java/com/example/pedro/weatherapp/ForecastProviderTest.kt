4f4166c0632d45f1de5e781e3883639f7e702acc
Pedro Roig <pedrorg18@hotmail.com>
2017-12-02 18:31:45 +0100

    Create another test for ForecastProvider with mockito (lesson 28)

--- app/src/test/java/com/example/pedro/weatherapp/ForecastProviderTest.kt
+++ app/src/test/java/com/example/pedro/weatherapp/ForecastProviderTest.kt
@@ -3,8 +3,10 @@ package com.example.pedro.weatherapp
 import com.example.pedro.weatherapp.domain.datasource.ForecastDataSource
 import com.example.pedro.weatherapp.domain.datasource.ForecastProvider
 import com.example.pedro.weatherapp.domain.model.Forecast
+import com.example.pedro.weatherapp.domain.model.ForecastList
 import org.junit.Assert
 import org.junit.Test
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.mock
 
 import org.mockito.Mockito.`when` as whenever
@@ -13,8 +15,7 @@ import org.mockito.Mockito.`when` as whenever
 class ForecastProviderTest {
 
 
-    @Test
-    fun testDataSourceReturnsValue(){
+    @Test fun testDataSourceReturnsValue(){
         val ds = mock(ForecastDataSource::class.java)
         whenever(ds.requestDayForecast(0)).then {
             Forecast(0, 0, "desc", 20, 0, "url")
@@ -25,5 +26,19 @@ class ForecastProviderTest {
         val dayFc = provider.requestForecast(0)
         Assert.assertNotNull(dayFc)
         Assert.assertEquals(dayFc.description, "desc")
+    }
+
+    @Test fun emptyDatabaseReturnsServerValue() {
+        val db = mock(ForecastDataSource::class.java)
+
+        val server = mock(ForecastDataSource::class.java)
+        whenever(server.requestForecastByZipCode(
+                any(Long::class.java), any(Long::class.java)))
+        .then{
+            ForecastList(0, "city", "country", listOf())
+        }
+
+        Assert.assertNotNull(
+                ForecastProvider(listOf(db, server)).requestByZipCode(0, 0))
     }
 }
